26657da361a8380b8ec8ee320f56e4e2672beb20
duc nguyen <ducknguyen14@gmail.com>
2020-08-13 03:16:04 -0700

    Customizing BottomNavigationView
    Trying to add icons onto the bottomnavigationbar

---
+++ app/src/main/java/com/example/instaclone/Utils/BottomNavigationViewHelper.java
@@ -0,0 +1,23 @@
+package com.example.instaclone.Utils;
+
+import android.util.Log;
+
+import com.ittianyu.bottomnavigationviewex.BottomNavigationViewEx;
+
+public class BottomNavigationViewHelper {
+    private static final String TAG = "BottomNavigationViewHel/DEBUG";
+
+    /**
+     * Notes: BottomNavigationView setup
+     */
+    public static void setupBottomNavigationView(BottomNavigationViewEx bottomNavigationViewEx)
+    {
+//        Log.d(TAG, "setupBottomNavigationView: Setting up BottomNavigationView");
+        bottomNavigationViewEx.enableAnimation(false);
+        bottomNavigationViewEx.enableItemShiftingMode(false);
+        bottomNavigationViewEx.enableShiftingMode(false);
+        bottomNavigationViewEx.setTextVisibility(false);
+
+    }
+
+}

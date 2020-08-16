70626480308143039d21af387a7081f1f7a10a61
duc nguyen <ducknguyen14@gmail.com>
2020-08-16 02:48:42 -0700

    SectionsPagerAdapter Tabs
    Created SectionsPagerAdapter class so that it can store fragments for tabs so that layout_center_viewpager.xml can swap out fragments for its tabs

--- app/src/main/java/com/example/instaclone/Home/HomeActivity.java
+++ app/src/main/java/com/example/instaclone/Home/HomeActivity.java
@@ -1,6 +1,7 @@
 package com.example.instaclone.Home;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.viewpager.widget.ViewPager;
 
 import android.content.Context;
 import android.os.Bundle;
@@ -10,6 +11,7 @@ import android.view.MenuItem;
 
 import com.example.instaclone.R;
 import com.example.instaclone.Utils.BottomNavigationViewHelper;
+import com.google.android.material.tabs.TabLayout;
 import com.ittianyu.bottomnavigationviewex.BottomNavigationViewEx;
 
 
@@ -32,6 +34,7 @@ public class HomeActivity extends AppCompatActivity {
         Log.d(TAG, "onCreate: Starting");
 
         setupBottomNavigationView();
+        setupViewPager();
     }
 
     /**
@@ -50,8 +53,36 @@ public class HomeActivity extends AppCompatActivity {
         Menu menu = bottomNavigationViewEx.getMenu();
         MenuItem menuItem = menu.getItem(ACTIVITY_NUM);
         menuItem.setChecked(true);
+    }
 
+    /**
+        Notes: Setup for adding the 3 tabs: Camera, Home, Messages
+     */
+    private void setupViewPager()
+    {
+        SectionsPagerAdapter adapter = new SectionsPagerAdapter(getSupportFragmentManager(), 1);
+
+        // Notes: Need to add fragments in this order
+        adapter.addFragment(new CameraFragment()); // Notes: Index 0
+        adapter.addFragment(new HomeFragment()); // Notes: Index 1
+        adapter.addFragment(new MessagesFragment()); // Notes: Index 2
+
+        // Notes: Setting ViewPager to Adapter
+        ViewPager viewPager = (ViewPager) findViewById(R.id.container);
+        viewPager.setAdapter(adapter);
+
+        // Notes: Setting tabLayout to viewPager
+        TabLayout tabLayout = (TabLayout) findViewById(R.id.tabs);
+        tabLayout.setupWithViewPager(viewPager);
+
+        // Notes: Setting icons according to their tabs
+        tabLayout.getTabAt(0).setIcon(R.drawable.ic_camera);
+        tabLayout.getTabAt(1).setIcon(R.drawable.ic_tea_logo);
+        tabLayout.getTabAt(2).setIcon(R.drawable.ic_messages);
 
     }
+
+
+
 
 }
---
+++ app/src/main/java/com/example/instaclone/Home/SectionsPagerAdapter.java
@@ -0,0 +1,45 @@
+package com.example.instaclone.Home;
+
+import android.widget.ProgressBar;
+
+import androidx.annotation.NonNull;
+import androidx.fragment.app.Fragment;
+import androidx.fragment.app.FragmentManager;
+import androidx.fragment.app.FragmentPagerAdapter;
+
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ *  Notes: This class stores fragments for tabs so that
+ *     layout_center_viewpager.xml can to swap out fragments for its tabs
+ */
+
+
+public class SectionsPagerAdapter extends FragmentPagerAdapter {
+    private static final String TAG = "SectionsPagerAdapter/DEBUG";
+
+    private final List<Fragment> mFragmentList = new ArrayList<>();
+
+    public SectionsPagerAdapter(@NonNull FragmentManager fm, int behavior) {
+        super(fm, behavior);
+    }
+
+
+    public void addFragment(Fragment fragment)
+    {
+        mFragmentList.add(fragment);
+    }
+
+
+    @NonNull
+    @Override
+    public Fragment getItem(int position) {
+        return mFragmentList.get(position);
+    }
+
+    @Override
+    public int getCount() {
+        return mFragmentList.size();
+    }
+}

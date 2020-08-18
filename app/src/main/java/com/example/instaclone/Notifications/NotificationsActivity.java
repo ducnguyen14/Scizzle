192a42791efd80331ba01c21991bc50f713ec572
duc nguyen <ducknguyen14@gmail.com>
2020-08-18 02:44:07 -0700

    Register Layout

--- app/src/main/java/com/example/instaclone/Notifications/NotificationsActivity.java
+++ app/src/main/java/com/example/instaclone/Notifications/NotificationsActivity.java
@@ -22,7 +22,7 @@ public class NotificationsActivity extends AppCompatActivity {
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activty_login);
+        setContentView(R.layout.activity_register);
 
         Log.d(TAG, "onCreate: started");
 

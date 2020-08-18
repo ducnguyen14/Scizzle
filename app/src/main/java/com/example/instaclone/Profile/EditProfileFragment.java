dc0e20bf82d6b9697049186a411cc1df26119115
duc nguyen <ducknguyen14@gmail.com>
2020-08-17 23:35:27 -0700

    universal Image Loader Config

--- app/src/main/java/com/example/instaclone/Profile/EditProfileFragment.java
+++ app/src/main/java/com/example/instaclone/Profile/EditProfileFragment.java
@@ -1,25 +1,61 @@
 package com.example.instaclone.Profile;
 
 import android.os.Bundle;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ImageView;
 
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
 
 import com.example.instaclone.R;
+import com.example.instaclone.Utils.UniversalImageLoader;
+import com.nostra13.universalimageloader.core.ImageLoader;
 
 public class EditProfileFragment extends Fragment {
     private static final String TAG = "EditProfileFragment/DEBUG";
+
+    private ImageView mProfilePhoto;
 
     @Nullable
     @Override
     public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState)
     {
         View view = inflater.inflate(R.layout.fragment_editprofile, container, false);
+        mProfilePhoto = (ImageView) view.findViewById(R.id.profile_photo);
+
+        initImageLoader();
+        setProfileImage();
+
 
         return view;
     }
+
+    /**
+     * Notes: Initialized the ImageLoader with its configurations
+     */
+    private void initImageLoader()
+    {
+        UniversalImageLoader universalImageLoader = new UniversalImageLoader(getActivity());
+        ImageLoader.getInstance().init(universalImageLoader.getConfig());
+    }
+
+
+
+    private void setProfileImage()
+    {
+        Log.d(TAG, "setProfileImage: setting profile image");
+
+        String imgURL = "i.pinimg.com/originals/19/58/7f/19587f4696f74eeea6f387816b9bff88.jpg";
+        UniversalImageLoader.setimage(imgURL,mProfilePhoto, null, "https://");
+
+    }
+
+
+
+
+
 }
---
+++ app/src/main/java/com/example/instaclone/Utils/UniversalImageLoader.java
@@ -0,0 +1,114 @@
+package com.example.instaclone.Utils;
+
+import android.content.Context;
+import android.graphics.Bitmap;
+import android.view.View;
+import android.widget.ImageView;
+import android.widget.ProgressBar;
+
+import com.example.instaclone.R;
+import com.nostra13.universalimageloader.cache.memory.impl.WeakMemoryCache;
+import com.nostra13.universalimageloader.core.DisplayImageOptions;
+import com.nostra13.universalimageloader.core.ImageLoader;
+import com.nostra13.universalimageloader.core.ImageLoaderConfiguration;
+import com.nostra13.universalimageloader.core.assist.FailReason;
+import com.nostra13.universalimageloader.core.assist.ImageScaleType;
+import com.nostra13.universalimageloader.core.display.FadeInBitmapDisplayer;
+import com.nostra13.universalimageloader.core.listener.ImageLoadingListener;
+
+public class UniversalImageLoader {
+    private static final int defaultImage = R.drawable.ic_profile;
+    private Context mContext;
+
+    public UniversalImageLoader(Context context)
+    {
+        mContext= context;
+    }
+
+    /**
+     * Notes: The way the universal image loader works is that it has an instance and the instance can be accessed anywhere on the application.
+     *      We set a bunch of settings one time and it can be used app-wide
+     * @return
+     */
+    public ImageLoaderConfiguration getConfig()
+    {
+        DisplayImageOptions defaultOptions = new DisplayImageOptions.Builder()
+                .showImageOnLoading(defaultImage)
+                // Notes: Default image shows if original image is unable to load
+                .showImageForEmptyUri(defaultImage)
+                // Notes: Default image shows if db image retrieval failed or on null input
+                .showImageOnFail(defaultImage)
+                .cacheOnDisk(true).cacheInMemory(true)
+                .cacheOnDisk(true).resetViewBeforeLoading(true)
+                .imageScaleType(ImageScaleType.EXACTLY)
+                .displayer(new FadeInBitmapDisplayer(300)).build();
+
+
+        ImageLoaderConfiguration configuration = new ImageLoaderConfiguration.Builder(mContext)
+                .defaultDisplayImageOptions(defaultOptions)
+                .memoryCache(new WeakMemoryCache())
+                .diskCacheSize(100 * 1024 * 1024).build();
+
+
+        return configuration;
+
+    }
+
+    /**
+     * Notes: This method is for setting a single image on a layout.
+     *      This method can be used to set images that are static.
+     *      It can't be used if the images are being changed in the Fragment/Activity - OR if they are being set in a
+     *      list or gridview!!!
+     */
+    public static void setimage(String imgURL, ImageView image, final ProgressBar mProgressBar, String append)
+    {
+        // Notes: Format = append + imgURL
+
+        ImageLoader imageLoader = ImageLoader.getInstance();
+
+        imageLoader.displayImage(append + imgURL, image, new ImageLoadingListener()
+        {
+            @Override
+            public void onLoadingStarted(String imageUri, View view)
+            {
+                // Notes: We want progress bar visible when loading starts
+                if(mProgressBar != null)
+                {
+                    mProgressBar.setVisibility(View.VISIBLE);
+                }
+            }
+
+            @Override
+            public void onLoadingFailed(String imageUri, View view, FailReason failReason)
+            {
+                if(mProgressBar != null)
+                {
+                    mProgressBar.setVisibility(View.GONE);
+                }
+            }
+
+            @Override
+            public void onLoadingComplete(String imageUri, View view, Bitmap loadedImage)
+            {
+                if(mProgressBar != null)
+                {
+                    mProgressBar.setVisibility(View.GONE);
+                }
+            }
+
+            @Override
+            public void onLoadingCancelled(String imageUri, View view)
+            {
+                if(mProgressBar != null)
+                {
+                    mProgressBar.setVisibility(View.GONE);
+                }
+            }
+        });
+
+    }
+
+
+
+
+}

7be29b13f1dabe88478387450a625eda156321d2
duc nguyen <ducknguyen14@gmail.com>
2020-08-22 22:09:34 -0700

    Verifying Permissions for Sharing

--- app/src/main/java/com/example/instaclone/Share/ShareActivity.java
+++ app/src/main/java/com/example/instaclone/Share/ShareActivity.java
@@ -1,6 +1,7 @@
 package com.example.instaclone.Share;
 
 import android.content.Context;
+import android.content.pm.PackageManager;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.Menu;
@@ -8,14 +9,19 @@ import android.view.MenuItem;
 
 import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.app.ActivityCompat;
 
 import com.example.instaclone.R;
 import com.example.instaclone.Utils.BottomNavigationViewHelper;
+import com.example.instaclone.Utils.Permissions;
 import com.ittianyu.bottomnavigationviewex.BottomNavigationViewEx;
 
 public class ShareActivity extends AppCompatActivity {
+
+    // Notes: Constants
     private static final String TAG = "ShareActivity/DEBUG";
     private static final int ACTIVITY_NUM = 2;
+    private static final int VERIFY_PERMISSIONS_REQUEST = 1;
 
     private Context mContext = ShareActivity.this;
 
@@ -27,8 +33,84 @@ public class ShareActivity extends AppCompatActivity {
 
         Log.d(TAG, "onCreate: started");
 
+        // Notes: Check to see if permissions are granted
+        if(checkPermissionsArray(Permissions.PERMISSIONS))
+        {
+
+        }
+        else
+        {
+            // Notes: Need to verify permissions
+            verifyPermissions(Permissions.PERMISSIONS);
+        }
+
+
         // Notes: Set ups
-        setupBottomNavigationView();
+//        setupBottomNavigationView();
+    }
+
+
+    public void verifyPermissions(String[] permissions)
+    {
+        Log.d(TAG, "verifyPermissions: verifying permissions.");
+
+        // Notes:
+        ActivityCompat.requestPermissions(ShareActivity.this, permissions, VERIFY_PERMISSIONS_REQUEST);
+    }
+
+
+
+    /**
+     * Notes: Checks an array of permissions
+     * @param permissions
+     * @return
+     */
+    public boolean checkPermissionsArray(String[] permissions)
+    {
+        Log.d(TAG, "\tcheckPermissionsArray: checking permissions array");
+
+        for(int i = 0; i < permissions.length; i++)
+        {
+            String check = permissions[i];
+
+            // Notes: Checks a single permission within the array of permissions
+            if(!checkPermissions(check))
+            {
+                return false;
+            }
+
+        }
+
+
+        return true;
+    }
+
+
+    /**
+     * Notes: Checks a single permission if it has been verified
+     * @param permission
+     * @return
+     */
+    public boolean checkPermissions(String permission)
+    {
+        Log.d(TAG, "\tcheckPermissions: checking permission: " + permission);
+
+        int permissionRequest = ActivityCompat.checkSelfPermission(ShareActivity.this, permission);
+
+        // Notes: Permission is not granted
+        if(permissionRequest != PackageManager.PERMISSION_GRANTED)
+        {
+            Log.d(TAG, "\tcheckPermissions: Permission was not granted for: " + permission);
+            return false;
+        }
+        else
+        {
+            Log.d(TAG, "\tcheckPermissions: Permission was granted for: " + permission);
+            return true;
+        }
+
+
+
     }
 
 

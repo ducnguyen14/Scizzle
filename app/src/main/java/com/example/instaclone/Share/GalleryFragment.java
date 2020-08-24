cb306daff764707db79383d325296b08d1c8dcc5
duc nguyen <ducknguyen14@gmail.com>
2020-08-23 23:41:44 -0700

    GalleryFragment Layout

--- app/src/main/java/com/example/instaclone/Share/GalleryFragment.java
+++ app/src/main/java/com/example/instaclone/Share/GalleryFragment.java
@@ -5,6 +5,11 @@ import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.GridView;
+import android.widget.ImageView;
+import android.widget.ProgressBar;
+import android.widget.Spinner;
+import android.widget.TextView;
 
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
@@ -15,6 +20,11 @@ import com.example.instaclone.R;
 public class GalleryFragment extends Fragment {
     private static final String TAG = "GalleryFragment/DEBUG";
 
+    // Notes: widgets
+    private GridView gridView;
+    private ImageView galleryImage;
+    private ProgressBar mProgressBar;
+    private Spinner directorySpinner;
 
     @Nullable
     @Override
@@ -22,6 +32,43 @@ public class GalleryFragment extends Fragment {
     {
         View view = inflater.inflate(R.layout.fragment_gallery, container, false);
         Log.d(TAG, "onCreateView: started");
+
+        galleryImage = (ImageView) view.findViewById(R.id.galleryImageView);
+        gridView = (GridView) view.findViewById(R.id.gridView);
+        directorySpinner = (Spinner) view.findViewById(R.id.spinnerDirectory);
+        mProgressBar = (ProgressBar) view.findViewById(R.id.progressBar);
+        mProgressBar.setVisibility(View.GONE);
+
+
+
+        ImageView shareClose = (ImageView) view.findViewById(R.id.ivCloseShare);
+        shareClose.setOnClickListener(new View.OnClickListener()
+        {
+            @Override
+            public void onClick(View v)
+            {
+                Log.d(TAG, "\tonClick: closing the gallery fragment.");
+
+                // Notes: Finishes activity/close fragment and takes us back to where we were before
+                getActivity().finish();
+            }
+        });
+
+        TextView nextScreen = (TextView) view.findViewById(R.id.tvNext);
+
+        nextScreen.setOnClickListener(new View.OnClickListener()
+        {
+            // Notes: Navigating to confirmation page
+            @Override
+            public void onClick(View v) {
+                Log.d(TAG, "\tonClick: navigating to the final share screen.");
+
+                // Notes: TODO - Need to write logic to handle coming from ShareActivity or EditProfileFragment
+            }
+        });
+
+
+
 
         return view;
     }
